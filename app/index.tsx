3001a07e72112aff119dce52eda5703857735c0e
immonroe <immanuelmonroe@gmail.com>
2025-08-15 15:43:03 -0400

    Feat: Added routing and dynamic page rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import "./global.css"
 import { Text, View } from "react-native"
+import { Link } from "expo-router"
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-5xl font-bold text-accent">Welcome!</Text>
+            <Link href="/onboarding">Onboarding</Link>
+            <Link href="/movie/avengers">Avenger Movie</Link>
         </View>
     )
 }
