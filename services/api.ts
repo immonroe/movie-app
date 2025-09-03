dbc0641bc955165c62ea0f1acfcf0151db0d8e08
immonroe <immanuelmonroe@gmail.com>
2025-09-03 08:34:02 -0400

    chore: added comment specifying commented out code as a reference to API docs

--- services/api.ts
+++ services/api.ts
@@ -27,6 +27,7 @@ export const fetchMovies = async ({ query }: { query: string }) => {
     return data.results
 }
 
+// copied from API docs for reference
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 // const options = {
 //   method: 'GET',
